e585d8053296e73aec7f3af47ad6e006350779f0
WJohnson <wjohnson.605@gmail.com>
2025-05-30 15:33:01 +0100

    Removed the reference to ICON_MICROPHONE.

--- esphome/components/i2s_audio/db_sound_meter.py
+++ esphome/components/i2s_audio/db_sound_meter.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, i2s_audio
-from esphome.const import CONF_ID, CONF_NAME, ICON_MICROPHONE, UNIT_DECIBEL
+from esphome.const import CONF_ID, CONF_NAME, UNIT_DECIBEL
 
 CONF_I2S_AUDIO_ID = "i2s_audio_id"
 
 db_sound_meter_ns = cg.esphome_ns.namespace("i2s_audio")
 DBSoundMeter = db_sound_meter_ns.class_("DBSoundMeter", cg.Component, sensor.Sensor)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_DECIBEL, ICON_MICROPHONE, 1).extend({
+CONFIG_SCHEMA = sensor.sensor_schema(UNIT_DECIBEL, "mdi:microphone", 1).extend({
     cv.GenerateID(): cv.declare_id(DBSoundMeter),
     cv.GenerateID(CONF_I2S_AUDIO_ID): cv.use_id(i2s_audio.I2SAudioComponent),
 })
