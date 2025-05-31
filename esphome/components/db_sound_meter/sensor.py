dcf179e1fa3b406a9d0aba6e8aefda4666a2cbca
WJohnson <wjohnson.605@gmail.com>
2025-05-31 05:03:12 +0100

    ?

--- esphome/components/db_sound_meter/sensor.py
+++ esphome/components/db_sound_meter/sensor.py
@@ -6,14 +6,14 @@ from esphome.const import UNIT_DECIBEL
 db_sound_meter_ns = cg.esphome_ns.namespace('db_sound_meter')
 DBSoundMeter = db_sound_meter_ns.class_('DBSoundMeter', cg.Component, sensor.Sensor)
 
-# Correctly declare mic_in as an ID object, not a string
-MIC_ID = cg.ID('mic_in')
+CONF_MIC_ID = 'mic_id'
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement=UNIT_DECIBEL,
     icon='mdi:microphone',
     accuracy_decimals=1,
 ).extend({
+    cv.Required(CONF_MIC_ID): cv.id(),  # Accept any ID, no strict type checking
     cv.GenerateID(): cv.declare_id(DBSoundMeter),
 })
 
@@ -21,6 +21,5 @@ async def to_code(config):
     var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
 
-    # Use the ID object for the microphone instance
-    mic = await cg.get_variable(MIC_ID)
+    mic = await cg.get_variable(config[CONF_MIC_ID])
     cg.add(var.register_callback(mic))
